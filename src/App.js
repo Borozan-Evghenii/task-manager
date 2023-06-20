08dbd869d237986fb6102e428827e51e01d97d49
Borozan Evghenii <borozan.eugen@gmail.com>
2023-06-20 05:49:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TodoApp from "./pages/TodoApp";
+
+function App() {
+  return (
+    <div className="container">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoApp(props) {
+  return (
+      <div>
+
+
+      </div>
+  );
+}
+
+export default TodoApp;
